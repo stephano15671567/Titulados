45adf82122f679d7b07a3e4604fe5ef496b0f7c0
stephano15671567 <stephano.almendra@alumnos.uv.cl>
2023-10-31 19:14:48 -0300

    avance

--- src/pages/Titulados/Titulados.jsx
+++ src/pages/Titulados/Titulados.jsx
@@ -1,150 +1,91 @@
 import React, { useState } from "react";
-import { Box, TextField, Button, Paper, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Button, Paper, Typography, TextField, Box } from "@mui/material";
+import BackgroundTransition from "../../BackgroundTransition/BackgroundTransition";
 import background1 from "../Home/components/images/imag_valparaiso.jpg";
 import background2 from "../Home/components/images/imagen_2.jpg";
 import background3 from "../Home/components/images/imagen_3.jpg";
 import background4 from "../Home/components/images/imagen_4.jpg";
 import background5 from "../Home/components/images/imagen_5.jpg";
-import BackgroundTransition from "../../BackgroundTransition/BackgroundTransition";
 
 function Titulados() {
-
-
-  // Función para manejar el envío del formulario de inicio de sesión
-const handleLoginSubmit = async (event) => {
-  event.preventDefault();
-
-  // Enviar una solicitud POST al servidor con las credenciales del usuario
-  const response = await fetch('/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ username, password }),  // Asumiendo que username y password están en el estado del componente
-  });
-
-  const data = await response.text();
-  if (response.ok) {
-    console.log('Inicio de sesión exitoso:', data);
-  } else {
-    console.error('Error en el inicio de sesión:', data);
-  }
-};
-
-// ...
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = (e) => {
+  const handleLoginSubmit = (e) => {
     e.preventDefault();
-
-    // Realiza la lógica de inicio de sesión aquí
-    if (username && password) {
-      // Envía una solicitud al servidor para iniciar sesión
-      // Maneja la respuesta del servidor y redirige si es necesario
-    }
+    // Aquí puedes manejar el inicio de sesión con nombre de usuario y contraseña si lo necesitas
   };
 
-  const containerStyle = {
-    display: "flex",
-    flexDirection: "column", // Apilar contenido verticalmente
-    alignItems: "center", // Centrar horizontalmente
-    justifyContent: "center", // Centrar verticalmente
-    minHeight: "100vh",
-    padding: "20px",
-    
+  const handleGoogleLogin = () => {
+    window.location.href = "http://localhost:4000/auth/google";
   };
 
-  const formContainerStyle = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: "20px",
-    background: "lightgray",
-    padding: "20px",
-   
-    width: "100%",
-    maxWidth: "600px",
-    
-  };
-
-  const leftTextStyle = {
-    fontSize: "15px",
-    
-  };
-
-  // ... (código previo)
-
-return (
-  <BackgroundTransition
-    images={[
-      background1,
-      background2,
-      background3,
-      background4,
-      background5,
-    ]}
-    duration={5000}
-  >
-    <Box style={containerStyle}>
-      <Paper elevation={3} style={formContainerStyle}>
-        <Typography variant="body1" style={leftTextStyle}>
-          
-          <br />
-        </Typography>
-        <Typography variant="h5" align="center" gutterBottom>
-          Iniciar Sesión Titulados
-        </Typography>
-        <form onSubmit={handleLogin}>
-          <TextField
-            label="Nombre de Usuario"
-            variant="outlined"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            fullWidth
-            margin="normal"
-            size="large"
-            
-          />
-          <TextField
-            label="Contraseña"
-            type="password"
-            variant="outlined"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            fullWidth
-            margin="normal"
-            size="large"
-          />
-          <Link to="/TituladosHome">
+  return (
+    <BackgroundTransition
+      images={[
+        background1,
+        background2,
+        background3,
+        background4,
+        background5,
+      ]}
+      duration={5000}
+    >
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <Paper elevation={10} sx={{ padding: 3, width: 300 }}>
+          <Typography variant="h5" gutterBottom textAlign="center">
+            Inicio de Sesión para Titulados
+          </Typography>
+          <form onSubmit={handleLoginSubmit}>
+            <TextField
+              label="Nombre de Usuario"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+              label="Contraseña"
+              type="password"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
             <Button
               type="submit"
               variant="contained"
-              
               fullWidth
-              size="large"
-              style={{
-                background: "rgba(0, 60, 88, 1)", // Cambia el color de fondo del botón
-                
-              }}
+              sx={{ marginTop: 2 }}
             >
               Iniciar Sesión
             </Button>
-          </Link>
-        </form>
-      </Paper>
-    </Box>
-  </BackgroundTransition>
-);
-
+          </form>
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{
+              marginTop: 2,
+              backgroundColor: "#4285F4",
+              "&:hover": {
+                backgroundColor: "#357ae8",
+              },
+            }}
+            onClick={handleGoogleLogin}
+          >
+            Iniciar Sesión con Google
+          </Button>
+        </Paper>
+      </Box>
+    </BackgroundTransition>
+  );
 }
 
 export default Titulados;
-
-
-
-// ...
-
-
--- src/pages/Titulados/Titulados_Home.jsx
+++ src/pages/Titulados/Titulados_Home.jsx
@@ -13,88 +13,84 @@ function TituladosHome() {
   };
 
   return (
-      <BackgroundTransition
-        images={[
-          background1,
-          background2,
-          background3,
-          background4,
-          background5,
-        ]}
-        duration={5000}
-      >
-    
-    <Container maxWidth="md">
-      <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-        justifyContent="center"
-        minHeight="100vh"
-        padding="20px"
-      >
-        <Paper elevation={3} style={{ padding: "20px", textAlign: "center", background: "lightgray", marginBottom: "20px" }}>
-          <Typography variant="h4" align="center" gutterBottom>
-            Bienvenido al Sistema de Titulación
-          </Typography>
+    <BackgroundTransition
+      images={[
+        background1,
+        background2,
+        background3,
+        background4,
+        background5,
+      ]}
+      duration={5000}
+    >
+      <Container maxWidth="md">
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          height="100vh"  // Cambiado de minHeight a height
+          padding="20px"
+        >
+          <Paper elevation={3} style={{ padding: "20px", textAlign: "center", background: "lightgray", marginBottom: "20px" }}>
+            <Typography variant="h4" align="center" gutterBottom>
+              Bienvenido al Sistema de Titulación
+            </Typography>
 
-          {/* Botón para subir tesis */}
-          <Typography variant="h5" gutterBottom>
-            Subir Tesis
-          </Typography>
-          <Typography variant="body1">
-            En el Sistema de Titulación, puedes subir tu tesis de forma sencilla.
-          </Typography>
-          <input
-            accept="application/pdf"
-            style={{ display: "none" }}
-            id="upload-tesis"
-            type="file"
-            onChange={handleFileUpload}
-          />
-          <label htmlFor="upload-tesis" style={{ width: '100%', marginTop: "20px" }}>
-            <Button
-              variant="contained"
-              style={{ backgroundColor: 'rgba(0, 60, 88, 1)' }}
-              fullWidth
-              component="span"  // Esto es clave para hacer que el botón funcione con el label
-            >
+            {/* Botón para subir tesis */}
+            <Typography variant="h5" gutterBottom>
               Subir Tesis
-            </Button>
-          </label>
+            </Typography>
+            <Typography variant="body1">
+              En el Sistema de Titulación, puedes subir tu tesis de forma sencilla.
+            </Typography>
+            <input
+              accept="application/pdf"
+              style={{ display: "none" }}
+              id="upload-tesis"
+              type="file"
+              onChange={handleFileUpload}
+            />
+            <label htmlFor="upload-tesis" style={{ width: '100%', marginTop: "20px" }}>
+              <Button
+                variant="contained"
+                style={{ backgroundColor: 'rgba(0, 60, 88, 1)' }}
+                fullWidth
+                component="span"  // Esto es clave para hacer que el botón funcione con el label
+              >
+                Subir Tesis
+              </Button>
+            </label>
 
-          {/* Botón para subir ficha de inscripción */}
-          <Typography variant="h5" gutterBottom>
-            Ficha de Inscripción
-          </Typography>
-          <Typography variant="body1">
-            Completa la ficha de inscripción para tu seminario de título.
-          </Typography>
-          <input
-            accept="application/pdf"
-            style={{ display: "none" }}
-            id="upload-ficha"
-            type="file"
-            onChange={handleFileUpload}
-          />
-          <label htmlFor="upload-ficha" style={{ width: '100%', marginTop: "20px" }}>
-            <Button
-              variant="contained"
-              style={{ backgroundColor: 'rgba(0, 60, 88, 1)' }}
-              fullWidth
-              component="span"
-            >
-              Subir Ficha de Inscripción
-            </Button>
-          </label>
-        </Paper>
-      </Box>
-      
-    </Container>
+            {/* Botón para subir ficha de inscripción */}
+            <Typography variant="h5" gutterBottom>
+              Ficha de Inscripción
+            </Typography>
+            <Typography variant="body1">
+              Completa la ficha de inscripción para tu seminario de título.
+            </Typography>
+            <input
+              accept="application/pdf"
+              style={{ display: "none" }}
+              id="upload-ficha"
+              type="file"
+              onChange={handleFileUpload}
+            />
+            <label htmlFor="upload-ficha" style={{ width: '100%', marginTop: "20px" }}>
+              <Button
+                variant="contained"
+                style={{ backgroundColor: 'rgba(0, 60, 88, 1)' }}
+                fullWidth
+                component="span"
+              >
+                Subir Ficha de Inscripción
+              </Button>
+            </label>
+          </Paper>
+        </Box>
+      </Container>
     </BackgroundTransition>
   );
-  
 }
-
 
 export default TituladosHome;
