5f12e155a9e3646d4a2dbfc9441c9031e81c96d6
stephano15671567 <stephano.almendra@alumnos.uv.cl>
2023-11-13 23:48:35 -0300

    archivos

--- src/pages/Secretarias/Secretarias.jsx
+++ src/pages/Secretarias/Secretarias.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Box, Typography, Button, Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import BackgroundTransition from '../../BackgroundTransition/BackgroundTransition';
 import background1 from '../Home/components/images/imag_valparaiso.jpg';
 import background2 from '../Home/components/images/imagen_2.jpg';
@@ -12,15 +13,14 @@ function SecretariaView() {
   const [titulados, setTitulados] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:4000/api/alumnosTitulados')
+    fetch('http://localhost:4000/api/titulados/alumnosTitulados')
       .then((response) => {
-        // Verifica si la respuesta es exitosa y si el contenido es tipo JSON
         if (response.ok && response.headers.get('Content-Type').includes('application/json')) {
           return response.json();
         }
-        // Si la respuesta no es exitosa, lanza un error
         throw new Error('La respuesta no es JSON: ' + response.statusText);
       })
       .then((data) => {
@@ -33,32 +33,45 @@ function SecretariaView() {
       });
   }, []);
 
-  const containerStyle = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    minHeight: "100vh",
-    padding: "20px",
-    fontFamily: "Times New Roman",
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
   };
 
-  const paperStyle = {
-    padding: "20px",
-    textAlign: "center",
-    marginBottom: "20px",
-    width: "100%",
-    maxWidth: "1000px",
-    background: "lightgray",
-    borderRadius: "10px",
-    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+  const handleFileUpload = async () => {
+    if (!selectedFile) {
+      alert('Por favor selecciona un archivo para subir');
+      return;
+    }
+  
+    const formData = new FormData();
+    formData.append('myFile', selectedFile);
+  
+    try {
+      await axios.post('http://localhost:4000/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      alert('Archivo subido con éxito');
+      // Aquí puedes implementar lógica adicional tras una carga exitosa
+    } catch (error) {
+      console.error('Error al subir el archivo:', error);
+      alert('Error al subir el archivo');
+    }
+  };
+  
+
+  const buttonSx = {
+    mt: 2,
+    mb: 2,
+    bgcolor: "rgba(0, 60, 88, 1)",
+    '&:hover': {
+      bgcolor: "rgba(0, 70, 100, 1)"
+    },
+    color: 'white'
   };
 
-  const buttonStyle = {
-    background: "rgba(0, 60, 88, 1)",
-    marginTop: "20px",
-    display: "block"
-  };
+  const reportUrl = 'http://localhost:4000/path-to-report'; // Reemplazar con el enlace correcto al reporte
 
   if (loading) {
     return <Typography>Cargando...</Typography>;
@@ -71,28 +84,69 @@ function SecretariaView() {
   return (
     <BackgroundTransition images={[background1, background2, background3, background4, background5]} duration={5000}>
       <Container maxWidth="md">
-        <Box style={containerStyle}>
-          <Paper elevation={3} style={paperStyle}>
+        <Box sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "100vh",
+          padding: "20px",
+          fontFamily: "Times New Roman",
+        }}>
+          <Paper sx={{
+            padding: "20px",
+            textAlign: "center",
+            marginBottom: "20px",
+            width: "100%",
+            maxWidth: "1000px",
+            bgcolor: "lightgray",
+            borderRadius: "10px",
+            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+          }}>
+            {/* Componente para seleccionar el archivo */}
+            <input type="file" onChange={handleFileChange} accept=".xlsx, .xls, .csv" style={{ margin: "20px 0" }} />
+            
+            {/* Botón para cargar el archivo */}
+            <Button variant="contained" fullWidth sx={buttonSx} onClick={handleFileUpload}>
+              Subir Archivo
+            </Button>
             <Typography variant="h5" gutterBottom>
-              Ver Tesis
+              Secretaría - Administración de Tesis y Documentos
             </Typography>
-            <Typography variant="body1">
-              Ver la lista de tesis y su estado.
-            </Typography>
+            
             <Link to="/ver-tesis" style={{ textDecoration: "none" }}>
-              <Button variant="contained" color="primary" fullWidth style={buttonStyle}>
+              <Button variant="contained" fullWidth sx={buttonSx}>
                 Ver Tesis
               </Button>
             </Link>
-            {/* ... otros botones y funcionalidades ... */}
+
+            <Link to="/generar-actas" style={{ textDecoration: "none" }}>
+              <Button variant="contained" fullWidth sx={buttonSx}>
+                Generar Actas
+              </Button>
+            </Link>
+
+            <Link to="/generar-comision" style={{ textDecoration: "none" }}>
+              <Button variant="contained" fullWidth sx={buttonSx}>
+                Generar Comisión
+              </Button>
+            </Link>
+
+            {/* Botón para ver el reporte */}
+            <a href={reportUrl} style={{ textDecoration: "none" }} download>
+              <Button variant="contained" fullWidth sx={buttonSx}>
+                Ver Reporte
+              </Button>
+            </a>
+
             <TableContainer component={Paper}>
-              <Table aria-label="simple table">
+              <Table aria-label="lista de titulados">
                 <TableHead>
                   <TableRow>
                     <TableCell>ID</TableCell>
                     <TableCell>Alumno</TableCell>
                     <TableCell>RUT</TableCell>
-                    {/* ... más celdas de encabezado ... */}
+                    {/* Añade más celdas de encabezado según sea necesario */}
                   </TableRow>
                 </TableHead>
                 <TableBody>
@@ -101,7 +155,7 @@ function SecretariaView() {
                       <TableCell>{titulado.id}</TableCell>
                       <TableCell>{titulado.alumno}</TableCell>
                       <TableCell>{titulado.rut}</TableCell>
-                      {/* ... más celdas con datos ... */}
+                      {/* Añade más celdas con datos según sea necesario */}
                     </TableRow>
                   ))}
                 </TableBody>
@@ -115,3 +169,5 @@ function SecretariaView() {
 }
 
 export default SecretariaView;
+
+
