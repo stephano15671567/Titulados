fad760b629c2c9fef948679fdc8bb8fae5802ee2
Step <stephano.almendra@gmail.com>
2023-11-18 23:07:39 -0300

    visualizacion profesores

--- src/pages/Secretarias/TableData.jsx
+++ src/pages/Secretarias/TableData.jsx
@@ -10,21 +10,33 @@ import {
   TablePagination,
   Select,
   MenuItem,
+  Button,
 } from '@mui/material';
+import axios from 'axios';
 
 function TableData({ titulados }) {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
-  // Guarda las selecciones de guía e informante por cada titulado
   const [selections, setSelections] = useState({});
   const [profesoresGuias, setProfesoresGuias] = useState([]);
   const [profesoresInformantes, setProfesoresInformantes] = useState([]);
 
-  // Carga inicial de datos de profesores
+
   useEffect(() => {
-    // Reemplaza con tu API real para obtener profesores guías e informantes
-    fetch('/api/profesores-guias').then(response => response.json()).then(data => setProfesoresGuias(data));
-    fetch('/api/profesores-informantes').then(response => response.json()).then(data => setProfesoresInformantes(data));
+    // Aquí es donde haces las llamadas a la API para cargar los datos de los profesores
+    axios.get('http://localhost:4000/api/profesores/guias/')
+      .then(response => {
+        console.log('Profesores guías cargados:', response.data);
+        setProfesoresGuias(response.data);
+      })
+      .catch(error => console.error('Error al cargar los profesores guías', error));
+
+    axios.get('http://localhost:4000/api/profesores/informantes/')
+      .then(response => {
+        console.log('Profesores informantes cargados:', response.data);
+        setProfesoresInformantes(response.data);
+      })
+      .catch(error => console.error('Error al cargar los profesores informantes', error));
   }, []);
 
   const handleChangePage = (event, newPage) => {
@@ -44,65 +56,86 @@ function TableData({ titulados }) {
         [type]: value,
       },
     }));
-    
-    // Opcional: Actualizar en la base de datos aquí o mediante un botón de "Guardar"
+  };
+
+  const saveAssignments = async () => {
+    const updatePromises = Object.entries(selections).map(([tituladoId, { guia, informante }]) =>
+      axios.post('/api/profesores/asignacion', {
+        alumnoId: tituladoId,
+        profesorGuiaId: guia,
+        informanteId: informante,
+      })
+    );
+
+    try {
+      await Promise.all(updatePromises);
+      alert('Asignaciones guardadas con éxito');
+    } catch (error) {
+      console.error('Error al guardar las asignaciones', error);
+      alert('Error al guardar las asignaciones');
+    }
   };
 
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="lista de titulados">
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>Alumno</TableCell>
-            <TableCell>RUT</TableCell>
-            <TableCell>Profesor Guía</TableCell>
-            <TableCell>Profesor Informante</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {titulados.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((titulado) => (
-            <TableRow key={titulado.id}>
-              <TableCell>{titulado.id}</TableCell>
-              <TableCell>{titulado.alumno}</TableCell>
-              <TableCell>{titulado.rut}</TableCell>
-              <TableCell>
-                <Select
-                  value={selections[titulado.id]?.guia || ''}
-                  onChange={(event) => handleSelectionChange(titulado.id, event.target.value, 'guia')}
-                  displayEmpty
-                >
-                  <MenuItem value=""><em>Ninguno</em></MenuItem>
-                  {profesoresGuias.map((profesor) => (
-                    <MenuItem key={profesor.id} value={profesor.id}>{profesor.nombre}</MenuItem>
-                  ))}
-                </Select>
-              </TableCell>
-              <TableCell>
-                <Select
-                  value={selections[titulado.id]?.informante || ''}
-                  onChange={(event) => handleSelectionChange(titulado.id, event.target.value, 'informante')}
-                  displayEmpty
-                >
-                  <MenuItem value=""><em>Ninguno</em></MenuItem>
-                  {profesoresInformantes.map((profesor) => (
-                    <MenuItem key={profesor.id} value={profesor.id}>{profesor.nombre}</MenuItem>
-                  ))}
-                </Select>
-              </TableCell>
+    <div>
+      <TableContainer component={Paper}>
+        <Table aria-label="lista de titulados">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Alumno</TableCell>
+              <TableCell>RUT</TableCell>
+              <TableCell>Profesor Guía</TableCell>
+              <TableCell>Profesor Informante</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <TablePagination
-        component="div"
-        count={titulados.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {titulados.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((titulado) => (
+              <TableRow key={titulado.id}>
+                <TableCell>{titulado.id}</TableCell>
+                <TableCell>{titulado.alumno}</TableCell>
+                <TableCell>{titulado.rut}</TableCell>
+                <TableCell>
+                  <Select
+                    value={selections[titulado.id]?.profesorGuia || ''}
+                    onChange={(event) => handleSelectionChange(titulado.id, event.target.value, 'profesorGuia')}
+                    displayEmpty
+                  >
+                    <MenuItem value=""><em>Ninguno</em></MenuItem>
+                    {profesoresGuias.map((profesor) => (
+                      <MenuItem key={profesor.id} value={profesor.id}>{profesor.nombre}</MenuItem>
+                    ))}
+                  </Select>
+                </TableCell>
+                <TableCell>
+                  <Select
+                    value={selections[titulado.id]?.informante || ''}
+                    onChange={(event) => handleSelectionChange(titulado.id, event.target.value, 'informante')}
+                    displayEmpty
+                  >
+                    <MenuItem value=""><em>Ninguno</em></MenuItem>
+                    {profesoresInformantes.map((profesor) => (
+                      <MenuItem key={profesor.id} value={profesor.id}>{profesor.nombre}</MenuItem>
+                    ))}
+                  </Select>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <TablePagination
+          component="div"
+          count={titulados.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </TableContainer>
+      <Button variant="contained" color="primary" onClick={saveAssignments}>
+        Guardar Asignaciones
+      </Button>
+    </div>
   );
 }
 
