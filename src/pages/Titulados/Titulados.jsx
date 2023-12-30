22b104bff9ee41e9fc85e79aa111977bb8e48e6b
Chii1914 <maximiliano.aguirre@alumnos.uv.cl>
2023-12-30 19:34:32 -0300

    Functional authetication

--- src/pages/Titulados/Titulados.jsx
+++ src/pages/Titulados/Titulados.jsx
@@ -13,23 +13,43 @@ import background5 from "../Home/components/images/imagen_5.jpg";
 function Titulados() {
   const win = window.sessionStorage; //Variable de sesión
   const [user, setUser] = useState({});
-  const [showSignIn, setShowSignIn] = useState(!win.getItem("token")); // Show if no token
-
-  const handleLoginSubmit = (e) => {
-    e.preventDefault();
-    // Aquí puedes manejar el inicio de sesión con nombre de usuario y contraseña si lo necesitas
-  };
-
-  const handleGoogleLogin = () => {
-    window.location.href = "http://localhost:4000/auth/google";
-  };
+  const [showSignIn, setShowSignIn] = useState(!win.getItem("status")); // Show if no token
+  
+  const verifyToken = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:4000/api/alumnos/ver/",
+        {},
+        {
+          headers: {
+            authorization: "Bearer " + win.getItem("token"),
+          },
+        }
+      );
+      if (res.data.status === true) {
+        win.setItem("status", res.data.status);
+        win.setItem("rol", res.data.rol);
+        win.setItem("token", res.data.token);
+        setUser({ rol: win.getItem("rol") });
+        console.log(user)
+        setShowSignIn(false);
+        return true;
+      }
+    } catch (err) {
+      win.clear();
+      setShowSignIn(true);
+      return false
+    }
+  }
 
   useEffect(() => {
-    if (win.getItem("token")) {
-      console.log("Token exists: ", win.getItem("token"));
-      const userObject = jwtDecode(win.getItem("token"));
-      setUser(userObject);
-      setShowSignIn(false); // Hide signIn button
+    if (win.getItem("status") === "true" && win.getItem("rol") === "alumno") {
+      if (win.getItem("token") !== null) {
+        verifyToken();
+      }
+    } else {
+      win.clear();
+      setShowSignIn(true);
     }
   }, []);
 
@@ -52,34 +72,29 @@ function Titulados() {
       google.accounts.id.prompt();
     }
   }, [showSignIn]);
-
+ 
   const handleToken = async (response) => {
-    console.log("token: ", response.credential);
     try {
-      const res = await axios.post('localhost:4000/api/alumnos/auth/', {
+      const res = await axios.post("http://localhost:4000/api/alumnos/auth/", {
         token: response.credential,
       });
-      console.log(res.data);
-      win.setItem("token", response.credential);
+      const usuario = jwtDecode(res.data);
+      /*SETEO DE CREDENCIALES*/
+      win.setItem("status", usuario.status);
+      win.setItem("rol", usuario.rol);
+      win.setItem("token", res.data);
+      setUser({ rol: usuario.rol });
       setShowSignIn(false); // Hide signIn button
       return true;
     } catch (err) {
       console.error("Error during token verification: ", err);
       return false;
-      // Handle the error
     }
   };
-  //GUARDAR TOKEN ENCRIPTADO EN VARIABLE DE CONTEXTO 
+  //GUARDAR TOKEN ENCRIPTADO EN VARIABLE DE CONTEXTO
 
   function handleCallbackResponse(response) {
-    console.log("token: ", response.credential); //Este token va al backend para verificar la autenticidad del usuario pero antes tiene que ir encriptado
-    const UserObject = jwtDecode(response.credential);
-    console.log(UserObject);
-    setUser(UserObject);
-    win.setItem("token", response.credential);
     handleToken(response);
-    
-    setShowSignIn(false); // Hide signIn button
   }
 
   function handleSignOut() {
@@ -121,11 +136,10 @@ function Titulados() {
         </Box>
       )}
 
-      {Object.keys(user).length !== 0 && (
+      {user.rol && (
         <>
           <div>
             <Outlet />
-            <img src={user.picture} alt="User" />
             <button onClick={handleSignOut}>Sign Out</button>
           </div>
         </>
